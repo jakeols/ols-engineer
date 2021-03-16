5596ba0a24dce310d53b1ae3abf97a84c345367a
Jake Ols <jake@kurtnoble.com>
2021-03-15 20:36:04 -0700

    drop medium

--- src/components/social.js
+++ src/components/social.js
@@ -42,7 +42,6 @@ const Social = () => {
             </a>
             <p>
                 running on <a style={{ textDecoration: 'none', color: 'rebeccapurple' }} href="https://www.strava.com/athletes/31979134">strava</a>.
-                writings on <a style={{ textDecoration: 'none', color: 'rebeccapurple' }} href="https://medium.com/@jakeols">medium</a>.
                 reading list <a style={{textDecoration: 'none', color: 'rebeccapurple'}} href="/books">here</a>.
             </p>
 
