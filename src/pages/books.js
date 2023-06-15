08e9c8e3332759037c7d3fcfbeeb7fd9b4f6b83f
Jake Ols <jakeo@thinkglobalschool.com>
2023-06-14 21:38:37 -0700

    add a bunch of books

--- src/pages/books.js
+++ src/pages/books.js
@@ -4,159 +4,186 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const BooksPage = () => (
-    <Layout>
-        <SEO title="Books" />
-        <h1>books</h1>
-        <p>i've been meaning to keep a reading log, so here it is:</p>
-        <h2>2023</h2>
-        <ul>
-            <li>dear edward - ann napolitano</li>
-            <li>a psalm for the wild-built - becky chambers</li>
-            <li>the tin drum - günter grass</li>
-        </ul>
-        <h2>2022</h2>
-        <ul>
-            <li>this is ear hustle - nigel poor & earlonne woods</li>
-            <li>jade city - fonda lee</li>
-            <li>random shit flying through the air - jackson ford</li>
-            <li>recursion - blacke crouch</li>
-            <li>eye of the shit storm - jackson ford</li>
-            <li>jade war - fonda lee</li>
-            <li>blacktop wasteland - sa crosby</li>
-            <li>rules of civility - amor towles</li>
-            <li>the maid - nita prose</li>
-            <li>death on the nile - agatha christie</li>
-            <li>razorblade tears - sa crosby</li>
-            <li>the turn of the key - ruth ware</li>
-            <li>the nickel boys - colson whitehead</li>
-            <li>fools errand - robin hobb</li>
-            <li>golden fool - robin hobb</li>
-            <li>fools fate - robin hobb</li>
-            <li>poppy wars - r.f. kuang</li>
-            <li>senlin ascends - josiah blancroft</li>
-            <li>the dragon republic - r.f. kuang</li>
-            <li>the burning god - r.f. kuang</li>
-            <li>jade legacy - fonda lee</li>
-            <li>the thursday murder club - richard osman</li>
-            <li>the way of kings - brandon sanderson</li>
-            <li>ready player one - ernest cline</li>
-            <li>words of radiance - brandon sanderson</li>
-            <li>how high we go in the dark - sequoia nagamatsu</li>
-            <li>oathbringer - brandon sanderson</li>
-            <li>the nix - nathan hill</li>
-            <li>rythm of war - brandon sanderson</li>
-            <li>remarkably bright creatures - shelby van pelt</li>
-            <li>scythe - neal shusterman</li>
-            <li>thunderhead - neal shusterman</li>
-            <li>the toll - neal shusterman</li>
-            <li>the searcher - tana french</li>
-            <li>a little life - hanya yanagihara</li>
-            <li>the man who died twice - richard osman</li>
-            <li>venemous lumpsucker - ned beauman</li>
-            <li>the bullet that missed - richard osman</li>
-            <li>babel - r.f. kuang</li>
-            <li>the sun down motel - simone st. james</li>
-            <li>lessons in chemistry - bonnie garmus</li>
-            <li>a natural history of dragons - marie brennan</li>
-            <li>sign here - claudia lux</li>
-            <li>dragon keeper - robin hobb</li>
-            <li>dragon haven - robin hobb</li>
-            <li>my lovel wife - samantha downing</li>
-            <li>legends & lattes - travis baldree</li>
-            <li>tomorrow and tomorrow and tomorrow - gabrielle zevin</li>
-            <li>the city we became - nk jemisin</li>
-            <li>the night swim - megan goldin</li>
-        </ul>
-        <h2>2021</h2>
-        <ul>
-            <li>mr. mercedes - stephen king</li>
-            <li>end of watch - stephen king</li>
-            <li>the couple next door - shari lapena</li>
-            <li>the brief and wondrous life of oscar wao - junot díaz</li>
-            <li>decoded - mai jia</li>
-            <li>mexican gothic - silvia moreno-garcia</li>
-            <li>a thousand splendid suns - khaled hosseini</li>
-            <li>the song of achilles - madeline miller</li>
-            <li>codex - lev grossman</li>
-            <li>the house on the cerulean sea - tj klune</li>
-            <li>assassins apprentice - robin hobb</li>
-            <li>ryal assassin - robin hobb</li>
-            <li>the midnight library - matt haig</li>
-            <li>the guest list - lucy foley</li>
-            <li>the changeling - victor lavalle</li>
-            <li>shadow and bone - leigh bardugo</li>
-            <li>siege and storm - leigh bardugo</li>
-            <li>ruin and rising - leigh bardugo</li>
-            <li>six of crows - leigh bardugo</li>
-            <li>crooked kingdom - leigh bardugo <i>(sensing a pattern?)</i></li>
-            <li>the vanishing half - brit bennett</li>
-            <li>ariadne - jennifer saint</li>
-            <li>vicious - ve schwab</li>
-            <li>vengeful - ve schwab</li>
-            <li>the gilded wolves - roshani choski</li>
-            <li>assassins quest - robin hobb</li>
-            <li>verity - colleen hoover</li>
-            <li>56 days - catherine ryan howard</li>
-            <li>a darker shade of magic - ve schwab</li>
-            <li>a gathering of shadows - ve schwab</li>
-            <li>a conjuring of light - ve schwab</li>
-            <li>american dirt - jeanine cummins</li>
-            <li>the night circus - erin morgenstern</li>
-            <li>a gentleman in moscow - amor towles</li>
-            <li>the silvered serpents - roshani choski</li>
-            <li>anxious people - fredrik backman</li>
-            <li>ace of spaces - faridah àbíké-íyímídé</li>
-            <li>ship of magic - robin hobb</li>
-            <li>convenience store woman - sayaka murata</li>
-            <li>mad ship - robin hobb</li>
-            <li>the girl who could move shit with her mind - jackson ford</li>
-            <li>the bronzed beasts - roshani choski</li>
-            <li>ship of destiny - robin hobb</li>
-            <li>dark matter - blake crouch</li>
-            <li>mistborn - brandon sanderson</li>
-            <li>the well of ascension - brandon sanderson</li>
-            <li>spindle splintered - alix harrow</li>
-            <li>the hero of ages - brandon sanderson</li>
-            <li>the invisible life of addie larue - v.e. schwab</li>
-            <li>project hail mary - andy weir</li>
-            <li>the atlas six - olivie blake</li>
-            <li>eleanor oliphant is completely fine - gail honeyman</li>
-            <li>the martian - andy weir</li>
-        </ul>
-        <h2>2020</h2>
-        <ul>
-            <li>annhilation (southern reach trilogy) book #1</li>
-            <li>killman creek - rachel caine</li>
-            <li>fishnet - kristin innes</li>
-            <li>authority (southern reach trilogy) book #2</li>
-            <li>acceptance (southern reach trilogy) book #3</li>
-            <li>1st to die - james patterson</li>
-            <li>first cut - judy melinek & tj mitchell</li>
-            <li>fool - christopher moore</li>
-            <li>running for my life - lopez lomong</li>
-            <li>the words of my father - yousef bashir</li>
-            <li>the boy on the wooden box - leon leyson</li>
-            <li>the 19th wife - david ebershoff</li>
-            <li>middlesex - jeffrey eugenides</li>
-            <li>what should be wild - julia fine</li>
-            <li>the spellman files - lisa lutz</li>
-            <li>borne - jeff vandermeer (auhtor of southern reach trilogy)</li>
-            <li>less - andrew greer</li>
-            <li>the swallows - lisa lutz</li>
-            <li>the woman in the window - aj finn</li>
-            <li>the lost girls of paris - pam jenoff</li>
-            <li>the saint of wolves and butchers - alex grecian</li>
-            <li>the girl of the train - paula hawkins</li>
-            <li>joyland - stephen king</li>
-            <li>the hours - michael cunningham</li>
-            <li>the martyred - richard e. kim</li>
-            <li>homeland - cory cotorow</li>
-            <li>little fires everywhere - celeste ng</li>
-            <li>noir - christopher moore</li>
-            <li>the uncommon appeal of clouds - alexander mccall smith</li>
-            <li>the mars room - rachel kushner</li>
-        </ul>
-    </Layout>
+  <Layout>
+    <SEO title="Books" />
+    <h1>books</h1>
+    <p>i've been meaning to keep a reading log, so here it is:</p>
+    <h2>2023</h2>
+    <ul>
+      <li>dear edward - ann napolitano</li>
+      <li>a psalm for the wild-built - becky chambers</li>
+      <li>the tin drum - günter grass</li>
+      <li>i'm glad my mom died - jeanette mccurdy </li>
+      <li>the atlas paradox - olivie blake</li>
+      <li>we begin at the end - chris whitaker </li>
+      <li>city of dragons - robin hobb</li>
+      <li>the blood of dragons - robin hobb</li>
+      <li>beartown - federick bachman (sp) </li>
+      <li>shuggie bain - douglas stuart</li>
+      <li>mrs demeanor - elinor lipman </li>
+      <li>flashback - dan simmons </li>
+      <li>a curious beginning - deanna raybourn</li>
+      <li>the great believers - rebecca makkai </li>
+      <li>a perilous undertaking- deanna raybourn</li>
+      <li>the lives of puppets - tj klune </li>
+      <li>the death of vivek oji - akwaeke emezi </li>
+      <li>tress of the emerald sea - brandon sanderson </li>
+      <li>the storied life of A.J. fickry</li>
+      <li>blessings - anna quindlen </li>
+      <li>divine rivals - rebecca ross </li>
+      <li>mistborn secret history - brandon sanderson </li>
+      <li>the 7 1/2 deaths of evelyn hardcastle - stuart turton </li>
+      <li> the mostly true story of tanner & louise - colleen oakley </li>
+      <li> the lincoln highway - amor towles</li>
+      <li>the prison healer - lynette noni </li>
+      <li> the gilded cage - lynette noni</li>
+      <li>the blood traitor - lynette noni</li>
+    </ul>
+    <h2>2022</h2>
+    <ul>
+      <li>this is ear hustle - nigel poor & earlonne woods</li>
+      <li>jade city - fonda lee</li>
+      <li>random shit flying through the air - jackson ford</li>
+      <li>recursion - blacke crouch</li>
+      <li>eye of the shit storm - jackson ford</li>
+      <li>jade war - fonda lee</li>
+      <li>blacktop wasteland - sa crosby</li>
+      <li>rules of civility - amor towles</li>
+      <li>the maid - nita prose</li>
+      <li>death on the nile - agatha christie</li>
+      <li>razorblade tears - sa crosby</li>
+      <li>the turn of the key - ruth ware</li>
+      <li>the nickel boys - colson whitehead</li>
+      <li>fools errand - robin hobb</li>
+      <li>golden fool - robin hobb</li>
+      <li>fools fate - robin hobb</li>
+      <li>poppy wars - r.f. kuang</li>
+      <li>senlin ascends - josiah blancroft</li>
+      <li>the dragon republic - r.f. kuang</li>
+      <li>the burning god - r.f. kuang</li>
+      <li>jade legacy - fonda lee</li>
+      <li>the thursday murder club - richard osman</li>
+      <li>the way of kings - brandon sanderson</li>
+      <li>ready player one - ernest cline</li>
+      <li>words of radiance - brandon sanderson</li>
+      <li>how high we go in the dark - sequoia nagamatsu</li>
+      <li>oathbringer - brandon sanderson</li>
+      <li>the nix - nathan hill</li>
+      <li>rythm of war - brandon sanderson</li>
+      <li>remarkably bright creatures - shelby van pelt</li>
+      <li>scythe - neal shusterman</li>
+      <li>thunderhead - neal shusterman</li>
+      <li>the toll - neal shusterman</li>
+      <li>the searcher - tana french</li>
+      <li>a little life - hanya yanagihara</li>
+      <li>the man who died twice - richard osman</li>
+      <li>venemous lumpsucker - ned beauman</li>
+      <li>the bullet that missed - richard osman</li>
+      <li>babel - r.f. kuang</li>
+      <li>the sun down motel - simone st. james</li>
+      <li>lessons in chemistry - bonnie garmus</li>
+      <li>a natural history of dragons - marie brennan</li>
+      <li>sign here - claudia lux</li>
+      <li>dragon keeper - robin hobb</li>
+      <li>dragon haven - robin hobb</li>
+      <li>my lovel wife - samantha downing</li>
+      <li>legends & lattes - travis baldree</li>
+      <li>tomorrow and tomorrow and tomorrow - gabrielle zevin</li>
+      <li>the city we became - nk jemisin</li>
+      <li>the night swim - megan goldin</li>
+    </ul>
+    <h2>2021</h2>
+    <ul>
+      <li>mr. mercedes - stephen king</li>
+      <li>end of watch - stephen king</li>
+      <li>the couple next door - shari lapena</li>
+      <li>the brief and wondrous life of oscar wao - junot díaz</li>
+      <li>decoded - mai jia</li>
+      <li>mexican gothic - silvia moreno-garcia</li>
+      <li>a thousand splendid suns - khaled hosseini</li>
+      <li>the song of achilles - madeline miller</li>
+      <li>codex - lev grossman</li>
+      <li>the house on the cerulean sea - tj klune</li>
+      <li>assassins apprentice - robin hobb</li>
+      <li>ryal assassin - robin hobb</li>
+      <li>the midnight library - matt haig</li>
+      <li>the guest list - lucy foley</li>
+      <li>the changeling - victor lavalle</li>
+      <li>shadow and bone - leigh bardugo</li>
+      <li>siege and storm - leigh bardugo</li>
+      <li>ruin and rising - leigh bardugo</li>
+      <li>six of crows - leigh bardugo</li>
+      <li>
+        crooked kingdom - leigh bardugo <i>(sensing a pattern?)</i>
+      </li>
+      <li>the vanishing half - brit bennett</li>
+      <li>ariadne - jennifer saint</li>
+      <li>vicious - ve schwab</li>
+      <li>vengeful - ve schwab</li>
+      <li>the gilded wolves - roshani choski</li>
+      <li>assassins quest - robin hobb</li>
+      <li>verity - colleen hoover</li>
+      <li>56 days - catherine ryan howard</li>
+      <li>a darker shade of magic - ve schwab</li>
+      <li>a gathering of shadows - ve schwab</li>
+      <li>a conjuring of light - ve schwab</li>
+      <li>american dirt - jeanine cummins</li>
+      <li>the night circus - erin morgenstern</li>
+      <li>a gentleman in moscow - amor towles</li>
+      <li>the silvered serpents - roshani choski</li>
+      <li>anxious people - fredrik backman</li>
+      <li>ace of spaces - faridah àbíké-íyímídé</li>
+      <li>ship of magic - robin hobb</li>
+      <li>convenience store woman - sayaka murata</li>
+      <li>mad ship - robin hobb</li>
+      <li>the girl who could move shit with her mind - jackson ford</li>
+      <li>the bronzed beasts - roshani choski</li>
+      <li>ship of destiny - robin hobb</li>
+      <li>dark matter - blake crouch</li>
+      <li>mistborn - brandon sanderson</li>
+      <li>the well of ascension - brandon sanderson</li>
+      <li>spindle splintered - alix harrow</li>
+      <li>the hero of ages - brandon sanderson</li>
+      <li>the invisible life of addie larue - v.e. schwab</li>
+      <li>project hail mary - andy weir</li>
+      <li>the atlas six - olivie blake</li>
+      <li>eleanor oliphant is completely fine - gail honeyman</li>
+      <li>the martian - andy weir</li>
+    </ul>
+    <h2>2020</h2>
+    <ul>
+      <li>annhilation (southern reach trilogy) book #1</li>
+      <li>killman creek - rachel caine</li>
+      <li>fishnet - kristin innes</li>
+      <li>authority (southern reach trilogy) book #2</li>
+      <li>acceptance (southern reach trilogy) book #3</li>
+      <li>1st to die - james patterson</li>
+      <li>first cut - judy melinek & tj mitchell</li>
+      <li>fool - christopher moore</li>
+      <li>running for my life - lopez lomong</li>
+      <li>the words of my father - yousef bashir</li>
+      <li>the boy on the wooden box - leon leyson</li>
+      <li>the 19th wife - david ebershoff</li>
+      <li>middlesex - jeffrey eugenides</li>
+      <li>what should be wild - julia fine</li>
+      <li>the spellman files - lisa lutz</li>
+      <li>borne - jeff vandermeer (auhtor of southern reach trilogy)</li>
+      <li>less - andrew greer</li>
+      <li>the swallows - lisa lutz</li>
+      <li>the woman in the window - aj finn</li>
+      <li>the lost girls of paris - pam jenoff</li>
+      <li>the saint of wolves and butchers - alex grecian</li>
+      <li>the girl of the train - paula hawkins</li>
+      <li>joyland - stephen king</li>
+      <li>the hours - michael cunningham</li>
+      <li>the martyred - richard e. kim</li>
+      <li>homeland - cory cotorow</li>
+      <li>little fires everywhere - celeste ng</li>
+      <li>noir - christopher moore</li>
+      <li>the uncommon appeal of clouds - alexander mccall smith</li>
+      <li>the mars room - rachel kushner</li>
+    </ul>
+  </Layout>
 )
 
 export default BooksPage
